61dd296bc5272a5e633f7c880f8ff6e3a9b56ed6
elsstrange <ellenabstrange@gmail.com>
2020-08-17 22:17:00 +0100

    Tests for managing card balance pass

--- lib/card.rb
+++ lib/card.rb
@@ -1,10 +1,26 @@
 class Card
-  CAPACITY = 90
+  MAX_VALUE = 90
+  BALANCE = 0
+
+  attr_reader :balance
 
   def initialize
+    @balance = BALANCE
   end
   
-  def capacity
+  def max_value
+    MAX_VALUE
   end
 
+  def add(money)
+    return 'You can only top up to £90 maximum' if too_much?(money)
+
+    @balance = money
+  end
+
+  private
+
+  def too_much?(money)
+    @balance + money > MAX_VALUE
+  end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -7,6 +7,23 @@ describe 'Card' do
     # execute
 
     # verify
-    expect(card.capacity).to eq 90
+    expect(card.max_value).to eq 90
+  end
+
+  it 'adds money' do
+    # set up
+    card = Card.new
+    money = 10
+    # execute
+    card.add(money)
+    # verify
+    expect(card.balance).to eq money
+  end
+
+  it 'doesn\'t allow too much money to be added' do
+    # set-up
+    card = Card.new
+    # verify
+    expect(card.add(100)).to eq 'You can only top up to £90 maximum'
   end
 end
